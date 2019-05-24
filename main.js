43be4e26d99e5fc1eb695fc9839eba33102c353b
Vitalii <ilovepinkpony1@gmail.com>
2019-05-24 11:45:13 +0300

    solution-rework

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 let list = document.querySelector('.tree');
 
 list.addEventListener('click', event => {
-if (event.target.tagName === 'SPAN') {
-  let hiddeEl = event.target.parentNode.querySelector('ul');
-    if (hiddeEl) {
-      hiddeEl.hidden = !hiddeEl.hidden;
-    }
+  if (event.target.tagName === 'SPAN') {
+    const hiddeEl = event.target.parentNode.querySelector('ul');
+      if (hiddeEl) {
+        hiddeEl.hidden = !hiddeEl.hidden;
+      }
   }
 });
